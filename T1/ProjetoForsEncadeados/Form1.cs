b28bbda1648d2ce0d3fb13de1411f9c94293e625
v011 <v011@benner.com.br>
2018-07-12 17:29:27 -0300

    Projeto co Fors encadeados

---
+++ T1/ProjetoForsEncadeados/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoForsEncadeados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int quantLinhas = 5;
+            string triangulo = "";
+            for (int linha = 1; linha <= quantLinhas; linha++)
+            {
+                for (int coluna = 1; coluna <= linha; coluna++)
+                {
+                    triangulo += (linha * coluna) + " ";
+                }
+                triangulo += "\n";
+            }
+            MessageBox.Show(triangulo);
+
+        }
+    }
+}
