d024d6ba2df35f2d0ca00bd0bdae6df71caefb88
v011 <v011@benner.com.br>
2018-07-12 15:15:14 -0300

    Exercicio que soma numeros de 1 a 1000

---
+++ T0/SomaNumeros/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SomaNumeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int somaNumeros = 0;
+            for (int valor=1; valor<=1000; valor++)
+            {
+                somaNumeros += valor;
+            }
+            MessageBox.Show("A soma é de: " + somaNumeros);
+        }
+    }
+}
