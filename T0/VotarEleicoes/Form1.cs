4773365ddbe83500e9538b99bcc2be4e8c7e7eec
v011 <v011@benner.com.br>
2018-07-12 15:03:20 -0300

    Exercicio com Projeto de votos

---
+++ T0/VotarEleicoes/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VotarEleicoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idade = 16;
+            bool brasileira = true;
+
+            if (idade >= 16 && brasileira == true)
+            {
+                MessageBox.Show("Você está apto a votar nas eleições");
+            }
+            else
+            {
+                MessageBox.Show("Você não pode votar");
+            }
+        }
+    }
+}
