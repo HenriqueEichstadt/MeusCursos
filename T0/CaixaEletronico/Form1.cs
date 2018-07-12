ebafe84687f333ad429a784c9051dcb218644f4f
v011 <v011@benner.com.br>
2018-07-12 14:58:35 -0300

    Projeto Caixa Eletronico

---
+++ T0/CaixaEletronico/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double valorInvestido = 2000.0;
+            int i = 1;
+            while(i<=12)
+            {
+                valorInvestido *= 1.01;
+                i += 1;
+            }
+                MessageBox.Show("O valor investido agora é de: " + valorInvestido);
+            }
+        }
+    }
