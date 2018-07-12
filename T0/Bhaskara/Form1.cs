b93b37cede5a76695db141a3c21cc2a6f0143b40
v011 <v011@benner.com.br>
2018-07-12 15:01:31 -0300

    Projeto Bhaskara

---
+++ T0/Bhaskara/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bhaskara
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 1;
+            int b = 12;
+            int c = -13;
+            double delta;
+            double a1;
+            double a2;
+            delta = b * b - 4 * a * c;
+            a1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            a2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+             MessageBox.Show("a1: " + a1, "a2: " + a2);
+        }
+    }
+}
