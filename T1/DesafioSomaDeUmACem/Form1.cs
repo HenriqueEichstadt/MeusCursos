ef98a29a109470c2200bda2114c145f9828d3a7e
v011 <v011@benner.com.br>
2018-07-13 13:28:34 -0300

    Desafio soma de um a cem

---
+++ T1/DesafioSomaDeUmACem/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesafioSomaDeUmACem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int somaNum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+               
+                if ( i % 3 != 0)
+                {
+                    somaNum += i;
+                }
+            }
+            MessageBox.Show("Os Numeros de 1 a 100 que não são multiplos de três: " + somaNum);
+        }
+    }
+}
