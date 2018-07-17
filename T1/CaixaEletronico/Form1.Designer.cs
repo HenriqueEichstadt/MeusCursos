6d53ed884d95535aad21e75bee1577265c525a0e
v011 <v011@benner.com.br>
2018-07-17 15:18:40 -0300

    Adicionando botão Depositar e botão Sacar

--- T1/CaixaEletronico/Form1.Designer.cs
+++ T1/CaixaEletronico/Form1.Designer.cs
@@ -34,6 +34,10 @@
             this.Titular = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.textoValor = new System.Windows.Forms.TextBox();
+            this.botaoDepositar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sacarValor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textoTitular
@@ -86,11 +90,53 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Número";
             // 
+            // textoValor
+            // 
+            this.textoValor.Location = new System.Drawing.Point(126, 258);
+            this.textoValor.Name = "textoValor";
+            this.textoValor.Size = new System.Drawing.Size(194, 20);
+            this.textoValor.TabIndex = 6;
+            this.textoValor.TextChanged += new System.EventHandler(this.textoValor_TextChanged);
+            // 
+            // botaoDepositar
+            // 
+            this.botaoDepositar.Location = new System.Drawing.Point(326, 255);
+            this.botaoDepositar.Name = "botaoDepositar";
+            this.botaoDepositar.Size = new System.Drawing.Size(75, 23);
+            this.botaoDepositar.TabIndex = 7;
+            this.botaoDepositar.Text = "Depositar";
+            this.botaoDepositar.UseVisualStyleBackColor = true;
+            this.botaoDepositar.Click += new System.EventHandler(this.botaoDepositar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Depósito";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // sacarValor
+            // 
+            this.sacarValor.Location = new System.Drawing.Point(407, 256);
+            this.sacarValor.Name = "sacarValor";
+            this.sacarValor.Size = new System.Drawing.Size(75, 23);
+            this.sacarValor.TabIndex = 9;
+            this.sacarValor.Text = "Sacar";
+            this.sacarValor.UseVisualStyleBackColor = true;
+            this.sacarValor.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.sacarValor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.botaoDepositar);
+            this.Controls.Add(this.textoValor);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.Titular);
@@ -114,6 +160,10 @@
         private System.Windows.Forms.Label Titular;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textoValor;
+        private System.Windows.Forms.Button botaoDepositar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button sacarValor;
     }
 }
 
--- T1/CaixaEletronico/Form1.cs
+++ T1/CaixaEletronico/Form1.cs
@@ -12,8 +12,17 @@ namespace CaixaEletronico
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private Conta conta;
+
+        // Método para exibir dados da conta
+        private void MostrarConta()
         {
+            textoTitular.Text = this.conta.Titular.Nome;
+            textoSaldo.Text = Convert.ToString(this.conta.Saldo);
+            textoNumero.Text = Convert.ToString(this.conta.Numero);
+        }
+        public Form1()
+        {            
             InitializeComponent();
         }
         private void button1_Click(object sender, EventArgs e)
@@ -28,17 +37,18 @@ namespace CaixaEletronico
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Conta conta = new Conta();
+            // criando nova conta
+            this.conta = new Conta();
             Cliente cliente = new Cliente();
-            conta.Titular = cliente;
-            conta.Titular.Nome = "Henrique Eichstädt";
-            conta.Deposita(123456);
-            conta.Numero = 987654321;
+            this.conta.Titular = cliente;
+            this.conta.Titular.Nome = "Henrique Eichstädt";
+            this.conta.Deposita(10000);
+            this.conta.Numero = 987654321;
+            this.conta.Titular.Idade = 18;
+            // método para exibir os dados da conta
+            this.MostrarConta();
 
 
-            textoTitular.Text = conta.Titular.Nome;
-            textoSaldo.Text = Convert.ToString(conta.Saldo);
-            textoNumero.Text = Convert.ToString(conta.Numero);
 
         }
 
@@ -48,6 +58,37 @@ namespace CaixaEletronico
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botaoDepositar_Click(object sender, EventArgs e)
+        {
+            // converte string para double 
+            string textoDoValorDoDeposito = textoValor.Text;
+            double valorDeposito = Convert.ToDouble(textoDoValorDoDeposito);
+            this.conta.Deposita(valorDeposito);
+
+            // Inicia o método que mostra as informações da conta
+            this.MostrarConta();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string textoDoValorSacado = textoValor.Text;
+            double valorSacado = Convert.ToDouble(textoDoValorSacado);
+            this.conta.Sacar(valorSacado);
+
+            // Inicia o método que mostra as informações da conta
+            this.MostrarConta();
+        }
+
+        private void textoValor_TextChanged(object sender, EventArgs e)
         {
 
         }
