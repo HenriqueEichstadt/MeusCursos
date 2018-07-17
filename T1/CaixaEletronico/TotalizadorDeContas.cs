7caf2f70623c06bd85db9d95b15a8860c0fae8eb
v011 <v011@benner.com.br>
2018-07-17 17:26:20 -0300

    Adicionando Classes, TotalizadorDeContas e ContaCorrente

---
+++ T1/CaixaEletronico/TotalizadorDeContas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class TotalizadorDeContas
+    {
+        public double SaldoTotal { get; private set; }
+        public void Adiciona(Conta conta)
+        {
+            this.SaldoTotal += conta.Saldo;
+        }
+    }
+}
