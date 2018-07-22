fa0eaf98db34d62c1e6753f42564b96a722d50b4
v011 <v011@benner.com.br>
2018-07-21 21:30:12 -0300

    Adicionando projeto LeituraDeDadosNoTerminal

---
+++ T1/Console Aplication LeituraDeDadosNoTerminal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Console_Aplication_LeituraDeDadosNoTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader leitorDeDados = Console.In;
+            string linha = leitorDeDados.ReadLine();
+            while (linha != null)
+            {
+                Console.WriteLine(linha);
+                linha = leitorDeDados.ReadLine();
+            }
+        }
+    }
+}
