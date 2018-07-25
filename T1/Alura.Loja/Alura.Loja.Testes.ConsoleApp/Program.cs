827cb63c70a105b7bc774452be8a33c1882fcf92
v011 <v011@benner.com.br>
2018-07-24 21:52:02 -0300

    Adicionando ChangeTracker

--- T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,80 +10,36 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //GravarUsandoAdoNet();
-            //GravarUsandoEntity();
-            //RecuperarProdutos();
-            //ExcluirProdutos();
-            //RecuperarProdutos();
-            AtualizarProduto();
-        }
-
-        private static void AtualizarProduto()
-        {
-            // Incluir um Produto
-            GravarUsandoEntity();
-            RecuperarProdutos();
-
-            // Atualizar o Produto
-            using (var repo = new ProdutoDAOEntity())
+            using (var contexto = new LojaContext())
             {
-                Produto primeiro = repo.Produtos().First();
-                primeiro.Nome = "Cassino Royalle - Editado";
-                repo.Atualizar(primeiro);
-
-            }
-            RecuperarProdutos();
-
-
-        }
-
-        private static void ExcluirProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                foreach (var item in produtos)
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
                 {
-                    repo.Remover(item);
+                    Console.WriteLine(p);
                 }
-            }
-        }
-
-        private static void RecuperarProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
-                foreach (var item in produtos)
+                Console.WriteLine("========================");
+                foreach (var e in contexto.ChangeTracker.Entries())
                 {
-                    Console.WriteLine(item.Nome);
+                    Console.WriteLine(e.State);
                 }
-            }
-        }
 
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Cassino Royalle";
-            p.Categoria = "Livros";
-            p.Preco = 49.90;
+                var p1 = produtos.Last();
+                p1.Nome = "007 - O Espião Que Me Amava";
 
-            using (var repo = new ProdutoDAO())
-            {
-                repo.Adicionar(p);
-            }
-        }
-        private static void GravarUsandoEntity()
-        {
-            Produto p = new Produto();
-            p.Nome = "Cassino Royalle";
-            p.Categoria = "Livros";
-            p.Preco = 49.90;
+                Console.WriteLine("========================");
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e.State);
+                }
 
-            using (var contexto = new ProdutoDAOEntity())
-            {
-                contexto.Adicionar(p);
+                //contexto.SaveChanges();
+
+                //Console.WriteLine("========================");
+                //produtos = contexto.Produtos.ToList();
+                //foreach (var p in produtos)
+                //{
+                //    Console.WriteLine(p);
+                //}
             }
         }
     }
