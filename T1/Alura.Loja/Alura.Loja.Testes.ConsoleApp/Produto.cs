b80f460b9fa87c2b84b5fba56da335d5bca31aca
v011 <v011@benner.com.br>
2018-07-26 11:30:53 -0300

    erro no Add-Migration

--- T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -5,11 +5,12 @@
         public int Id { get; internal set; }
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
-        public double Preco { get; internal set; }
+        public double PrecoUnitario { get; internal set; }
+        public string Unidade { get; internal set; }
 
         public override string ToString()
         {
-            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.Preco}";
+            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.PrecoUnitario}";
         }
     }
 }
--- T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,46 +11,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
 
-                var produtos = contexto.Produtos.ToList();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                var novoProduto = new Produto()
-                {
-                    Nome = "Sabão em pó",
-                    Categoria = "limpeza",
-                    Preco = 4.99
-                };
-
-                contexto.Produtos.Add(novoProduto);
-
-                //var p1 = produtos.First();
-                //contexto.Produtos.Remove(p1);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                contexto.Produtos.Remove(novoProduto);
-                                
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                //contexto.SaveChanges();
-
-                var entry = contexto.Entry(novoProduto);
-                Console.WriteLine("/n/n" + entry.Entity.ToString() + "   -   " + entry.State);
-
-            }
-        }
-
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-        {
-            Console.WriteLine("===================");
-            foreach (var e in entries)
-            {
-                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-            }
         }
     }
 }
