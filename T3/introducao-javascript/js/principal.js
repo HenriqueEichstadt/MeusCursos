b4455c867308cadd0898b95c3ce9980b7244ec41
v011 <v011@benner.com.br>
2018-09-10 17:14:27 -0300

    Terminando aula 5

--- T3/introducao-javascript/js/principal.js
+++ T3/introducao-javascript/js/principal.js
@@ -32,42 +32,13 @@
             }
 
             if (pesoEhValido && alturaEhValida) {
-                var imc = peso / (altura * altura);
-                tdImc.textContent = imc.toFixed(2);
+                var imc = calculaIMC(peso, altura);
+                tdImc.textContent = imc;
             }
         }
 
-        var botaoAdicionar = document.querySelector("#adicionar-paciente");
-        botaoAdicionar.addEventListener("click", function (event) {
-            event.preventDefault();
-
-            var form = document.querySelector("#form-adiciona");
-
-            var nome = form.nome.value;
-            var peso = form.peso.value;
-            var altura = form.altura.value;
-            var gordura = form.gordura.peso;
-            
-            var pacienteTr = document.createElement("tr");
-
-            var nomeTd = document.createElement("td");
-            var pesoTd = document.createElement("td");
-            var alturaTd = document.createElement("td");
-            var gorduraTd = document.createElement("td");
-            //var imcTd = document.createElement("td");
-
-            nomeTd.textContent = nome;
-            pesoTd.textContent = peso;
-            alturaTd.textContent = altura;
-            gorduraTd.textContent = gordura;
-            //imcTd.textContent = imc; 
-            
-            pacienteTr.appendChild(nomeTd);
-            pacienteTr.appendChild(pesoTd);
-            pacienteTr.appendChild(alturaTd);
-            pacienteTr.appendChild(gorduraTd);
-            
-            var tabela = document.querySelector("#tabela-pacientes");
-            tabela.appendChild(pacienteTr);
-            
-        });
+function calculaIMC(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
