104ebf680bff2d503800bfbd22734e4d7df1297d
v011 <v011@benner.com.br>
2018-09-11 11:59:50 -0300

    Terminando aula 8

--- T3/introducao-javascript/js/form.js
+++ T3/introducao-javascript/js/form.js
@@ -1,5 +1,5 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event) {
+botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
@@ -8,12 +8,35 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     var pacienteTr = montaTr(paciente);
 
+    var erros = validaPaciente(paciente);
+
+    if (erros.length > 0) {
+       exibeMensagensDeErro(erros);
+        return;
+    }
+
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });
+
+
+function exibeMensagensDeErro(erros){
+     var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+    
+     erros.forEach(function(erro){
+         var li = document.createElement("li");
+         li.textContent = erro;
+         ul.appendChild(li);
+     });
+    
+}
+
 
 function obtemPacienteDoFormulario(form) {
 
@@ -47,4 +70,19 @@ function montaTd(dado, classe) {
     td.textContent = dado;
 
     return td;
+}
+
+
+function validaPaciente(paciente) {
+
+    var erros = [];
+
+    if (!validaPeso(paciente.peso)) erros.push("O peso é inválido!");
+    if (!validaAltura(paciente.altura)) erros.push("A altura é inválida!");
+    if (paciente.nome.length == 0) erros.push("O nome do paciente não pode ser em branco!");
+    if (paciente.gordura.length == 0) erros.push("A gordura do paciente não pode ser em branco!");
+    if (paciente.peso.length == 0) erros.push("O peso do paciente não pode ser em branco!");
+
+    
+    return erros;
 }
--- T3/introducao-javascript/js/principal.js
+++ T3/introducao-javascript/js/principal.js
@@ -14,17 +14,17 @@
             var peso = tdPeso.textContent;
             var altura = tdAltura.textContent;
 
-            var alturaEhValida = true;
-            var pesoEhValido = true;
+            var alturaEhValida = validaAltura(altura);
+            var pesoEhValido = validaPeso(peso);
 
-            if (peso <= 0 || peso > 1000) {
+            if (!pesoEhValido) {
                 console.log("Peso inválido");
                 tdImc.textContent = "Peso inválido!";
                 pesoEhValido = false;
                 paciente.classList.add("paciente-invalido");
             }
 
-            if (altura <= 0 || altura >= 1000) {
+            if (!alturaEhValida) {
                 console.log("Altura inválida");
                 tdImc.textContent = "Altura inválida!";
                 alturaEhValida = false;
@@ -42,3 +42,28 @@ function calculaIMC(peso, altura){
     imc = peso / (altura * altura);
     return imc.toFixed(2);
 }
+
+
+function validaPeso(peso){
+    
+    if(peso >= 0 && peso < 1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && altura <= 3.0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
+
+
+
+
