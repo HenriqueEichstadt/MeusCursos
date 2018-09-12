cab243181dc50fe1596cd8ac28a4f48b94d5e8c0
v011 <v011@benner.com.br>
2018-09-12 16:01:30 -0300

    Terminando curso de REGEX

---
+++ T3/regex/CSharpREGEX/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSharpREGEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alvo = "11a22b33c";
+            string pattern = @"(\d\d)(\w)";
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(alvo);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[0].Value);
+                Console.WriteLine(match.Groups[1].Value);
+                Console.WriteLine(match.Groups[2].Value);
+                Console.WriteLine(match.Index);
+                Console.WriteLine(match.Length);
+            }
+        }
+    }
+}
