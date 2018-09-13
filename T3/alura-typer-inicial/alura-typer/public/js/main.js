2a99ebbde8398b6a2bba4c781133989d727ee744
v011 <v011@benner.com.br>
2018-09-13 10:24:16 -0300

    Adicionando projeto JQuery1

---
+++ T3/alura-typer-inicial/alura-typer/public/js/main.js
@@ -0,0 +1,16 @@
+// Contador de palavras 
+var frase = $(".frase").text();
+var numPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);
+
+// Contador de Palavras e Caracteres digitados
+var campo = $(".campo-digitacao");
+campo.on("input", function () {
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
