98e5352607f4e08da0d3dd8d103c8ee53eb006f0
v011 <v011@benner.com.br>
2018-09-13 17:28:56 -0300

    Metade da aula 6

--- T3/alura-typer-inicial/alura-typer/public/js/main.js
+++ T3/alura-typer-inicial/alura-typer/public/js/main.js
@@ -7,6 +7,7 @@ $(function () {
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
 });
 
@@ -41,12 +42,39 @@ function inicializaCronometro() {
 
             $("#tempo-digitacao").text(tempoRestante);
             if (tempoRestante < 1) {
-                campo.attr("disabled", true);
                 clearInterval(cronometroID);
+                finalizaJogo();
             }
         }, 1000);
     });
 }
+
+function finalizaJogo(){
+    campo.attr("disabled", true);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
+
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function () {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0, digitado.length);
+
+        if (digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+}
+
+$(".botao-remover").click(function(event){  // Erro aqui
+    event.preventDefault();
+    $(this).remove();
+});
 
 function reiniciaJogo() {
     campo.attr("disabled", false);
@@ -55,4 +83,22 @@ function reiniciaJogo() {
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
     inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+}
+
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Douglas";
+    var numPalavras = $("#contador-palavras").text();
+    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>"
+
+    var linha = "<tr>"+
+                    "<td>"+ usuario + "</td>"+
+                    "<td>"+ numPalavras + "</td>"+
+                    "<td>"+ botaoRemover + "</td>"+
+                "</tr>";
+
+                corpoTabela.prepend(linha);
 }
