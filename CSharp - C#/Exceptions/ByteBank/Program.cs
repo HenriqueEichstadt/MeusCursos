24eb4a48b6cab352fd2a7a44ef7884c4a877c252
henrique.eichstadt <v011@benner.com.br>
2019-02-16 19:29:11 -0200

    Adicionando projeto de Excecoes

---
+++ CSharp - C#/Exceptions/ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var conta = new ContaCorrente(654354, 54654);
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            Console.ReadLine();
+        }
+    }
+}
