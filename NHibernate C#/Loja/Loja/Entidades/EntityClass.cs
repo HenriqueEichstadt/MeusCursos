b463be1a269e03452e9d3d313d69065134d5e84b
henrique.eichstadt <v011@benner.com.br>
2019-04-09 05:46:30 -0300

    excluindo classes

---
+++ NHibernate C#/Loja/Loja/Entidades/EntityClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Loja.Entidades
+{
+    [DataContract]
+    class EntityClass
+    {
+        [DataMember]
+        public int Id { get; protected set; }
+    }
+}
+}
