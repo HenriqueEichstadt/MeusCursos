4788455cddfdbe5c41e7a27948f319896ac5193f
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2020-04-10 21:30:05 -0300

    Convertento projeto Xamarin Forms para android 10.0 "Android Q"

---
+++ Xamarin/Curso 1/TestDrive/TestDrive/TestDrive/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TestDrive.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestDrive
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ListagemView());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
