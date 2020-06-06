9e1808e7e4bb773116f03a90cf8d9d0d8724fd12
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2020-06-06 15:37:59 -0300

    Adicionando projeto para iniciar curso de certificação C# Programming - Parte 2

---
+++ CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 2 - Consumir tipos/certificacao-csharp-pt2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class Program
+    {
+        static IList<MenuItem> menuItems;
+        static void Main(string[] args)
+        {
+            IAulaItem itemSelecionado;
+            menuItems = GetMenuItems();
+
+            while (true)
+            {
+                ImprimirMenuItems(menuItems);
+                var opcao = Console.ReadLine();
+
+                int.TryParse(opcao, out int valorOpcao);
+
+                if (valorOpcao == 0)
+                {
+                    break;
+                }
+
+                if (valorOpcao > menuItems.Count)
+                {
+                    break;
+                }
+
+                itemSelecionado = Executar(valorOpcao);
+                Console.ReadKey();
+            }
+        }
+
+        private static IAulaItem Executar(int valorOpcao)
+        {
+            IAulaItem itemSelecionado;
+            MenuItem menuItem = menuItems[valorOpcao - 1];
+            Type tipoClasse = menuItem.TipoClasse;
+            itemSelecionado = (IAulaItem)Activator.CreateInstance(tipoClasse);
+
+            Console.WriteLine();
+            string titulo = $"EXECUTANDO: {menuItem.Titulo}";
+            Console.WriteLine(titulo);
+            Console.WriteLine(new string('=', titulo.Length));
+
+            itemSelecionado.Executar();
+            Console.WriteLine();
+            Console.WriteLine("Tecle algo para continuar...");
+            return itemSelecionado;
+        }
+
+        private static void ImprimirMenuItems(IList<MenuItem> menuItems)
+        {
+            int i = 1;
+            Console.WriteLine("SELECIONE UMA OPÇÃO");
+            Console.WriteLine("===================");
+            Console.WriteLine("0 - Sair");
+            foreach (var menuItem in menuItems)
+            {
+                Console.WriteLine((i++).ToString() + " - " + menuItem.Titulo);
+            }
+        }
+
+        private static IList<MenuItem> GetMenuItems()
+        {
+            return new List<MenuItem>
+            {
+                new MenuItem("Boxing", typeof(Boxing)),
+                new MenuItem("Unboxing", typeof(Unboxing)),
+                //new MenuItem("Conversoes Implícitas", typeof(ConversoesImplicitas)),
+                //new MenuItem("Conversoes Explícitas", typeof(ConversoesExplicitas)),
+                //new MenuItem("Operadores IS e AS", typeof(OperadoresISeAS)),
+                //new MenuItem("Operadores de Conversão", typeof(OperadoresDeConversao)),
+                //new MenuItem("Métodos Auxiliares de Conversão", typeof(MetodosAuxiliares)),
+                //new MenuItem("Usando Dynamic", typeof(UsandoDynamic)),
+                //new MenuItem("Conversões de Dynamic", typeof(ConversoesDeDynamic)),
+                //new MenuItem("Resolucão de Sobrecarga", typeof(ResolucaoSobrecarga)),
+                //new MenuItem("Usando ExpandObject", typeof(UsandoExpandObject)),
+                //new MenuItem("Interoperabilidade COM", typeof(InteropCOM))                
+            };
+        }
+    }
+
+    class MenuItem
+    {
+        public MenuItem(string titulo, Type tipoClasse)
+        {
+            Titulo = titulo;
+            TipoClasse = tipoClasse;
+        }
+
+        public string Titulo { get; set; }
+        public Type TipoClasse { get; set; }
+    }
+}
