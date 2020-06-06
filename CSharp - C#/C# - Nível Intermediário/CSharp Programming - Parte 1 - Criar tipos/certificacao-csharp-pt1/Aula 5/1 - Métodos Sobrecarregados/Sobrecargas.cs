0539f083ef5baad4d4c346e5a83973cc43ac6c84
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2020-06-06 14:56:19 -0300

    Curso 1 C# Programming - Aula 5 - Fim do curso

---
+++ CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 1 - Criar tipos/certificacao-csharp-pt1/Aula 5/1 - Métodos Sobrecarregados/Sobrecargas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class Sobrecargas : IAulaItem
+    {
+        public void Executar()
+        {
+            //VOLUME DO CUBO = lado ^ 3;
+            int lado = 3;
+            Console.WriteLine($"VolumeDoCubo: {Volume(lado)}");
+
+            //VOLUME DO CILINDRO = altura * PI * raio ^ 2;
+            double raio = 2;
+            int altura = 10;
+            Console.WriteLine($"VolumeDoCilindro: {Volume(altura, raio)}");
+
+            //VOLUME DO PRISMA = largura * profundidade * altura
+            long largura = 10;
+            altura = 6;
+            int profundidade = 4;
+            Console.WriteLine($"VolumeDoPrisma: {Volume(largura, profundidade, altura)}");
+        }
+
+        double Volume(double lado)
+        {
+            return Math.Pow(lado, 3);
+        }
+
+        double Volume(double altura, double raio)
+        {
+            return altura * Math.PI * Math.Pow(raio, 2);
+        }
+
+        double Volume(double largura, double profundidade, double altura)
+        {
+            return largura * profundidade * altura;
+        }
+
+    }
+}
