88c7a27540a829b75526b4e35bd66f9e02fa02d0
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2020-06-16 13:25:00 -0300

    Curso 3 C# Programming - Aula 2

---
+++ CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 3 - Encapsulamento e Hierarquia/Topico2.ProjetarInterface/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Topico2.ProjetarInterface
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            IEletrodomestico eletro1 = new Televisao();
+            IEletrodomestico eletro2 = new Abajur();
+            IEletrodomestico eletro3 = new Lanterna();
+            IEletrodomestico eletro4 = new Radio();
+
+            eletro1 = new Abajur();
+            eletro2 = new Televisao();
+            eletro3 = new Radio();
+            eletro4 = new Lanterna();
+        }
+
+        interface IEletrodomestico
+        {
+            event EventHandler Ligou;
+            event EventHandler Desligou;
+            void Desligar();
+            void Ligar();
+        }
+
+        interface IIluminacao
+        {
+            double PotenciaDaLampada { get; set; }
+        }
+
+        interface IRadioReceptor
+        {
+            double Frequencia { get; set; }
+        }
+        
+        class Televisao : IEletrodomestico, IRadioReceptor
+        {
+            public event EventHandler Ligou;
+            public event EventHandler Desligou;
+
+            public double Frequencia { get; set; }
+
+            public void Desligar()
+            {
+            }
+
+            public void Ligar()
+            {
+                if (Ligou != null)
+                {
+                    Ligou(this, new EventArgs());
+                }
+            }
+        }
+        
+        class Radio : IEletrodomestico, IRadioReceptor
+        {
+            public event EventHandler Ligou;
+            public event EventHandler Desligou;
+
+            public double Frequencia { get; set; }
+
+            public void Desligar()
+            {
+            }
+
+            public void Ligar()
+            {
+            }
+        }
+
+        class Abajur : IEletrodomestico, IIluminacao
+        {
+            public double PotenciaDaLampada { get; set; }
+
+            public event EventHandler Ligou;
+            public event EventHandler Desligou;
+
+            public void Desligar()
+            {
+            }
+
+            public void Ligar()
+            {
+            }
+        }
+
+        class Lanterna : IEletrodomestico, IIluminacao
+        {
+            public double PotenciaDaLampada { get; set; }
+
+            public event EventHandler Ligou;
+            public event EventHandler Desligou;
+
+            public void Desligar()
+            {
+            }
+
+            public void Ligar()
+            {
+            }
+        }
+    }
+}
