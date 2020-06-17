bc0e6cea1a794d5de80213cd02516e9973114fdc
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2020-06-16 23:37:14 -0300

    Curso 3 C# Programming - Aula 4

---
+++ CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 3 - Encapsulamento e Hierarquia/Topico4.ClasseBase/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Topico4.ClasseBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario(1500);
+            funcionario.CPF = "123.456.789-00";
+            funcionario.Nome = "josé da silva";
+            funcionario.DataNascimento = new DateTime(2000, 1, 1);
+
+            ((IFuncionario)funcionario).CargaHorariaMensal = 168;
+            ((IPlantonista)funcionario).CargaHorariaMensal = 32;
+            funcionario.EfeturarPagamento();
+            funcionario.CrachaGerado += (s, e) =>
+            {
+                Console.WriteLine("Crachá gerado");
+            };
+            ((IFuncionario)funcionario).GerarCracha();
+            ((IPlantonista)funcionario).GerarCracha();
+
+            Cliente cliente = new Cliente()
+            {
+                CPF = "789.456.123-99",
+                DataNascimento = new DateTime(1980, 1, 1),
+                Nome = "Maria de Souza",
+                DataUltimaCompra = new DateTime(2018, 1 ,1),
+                ValorUltimaCompra = 200
+            };
+
+            Console.WriteLine($"cliente: {cliente}");
+
+            Pessoa pessoa = new Cliente
+            {
+                CPF = "789.456.123-99",
+                DataNascimento = new DateTime(1980, 1, 1),
+                Nome = "Maria de Souza"
+            };
+            
+        }
+    }
+
+    interface IFuncionario
+    {
+        string CPF { get; set; }
+        string Nome { get; set; }
+        DateTime DataNascimento { get; set; }
+
+        event EventHandler CrachaGerado;
+
+        void GerarCracha();
+
+        decimal Salario { get; }
+        int CargaHorariaMensal { get; set; }
+
+        void EfeturarPagamento();
+    }
+
+    interface IPlantonista
+    {
+        int CargaHorariaMensal { get; set; }
+        void GerarCracha();
+    }
+
+    class Funcionario : Pessoa, IFuncionario, IPlantonista
+    {
+        public event EventHandler CrachaGerado;
+
+        void IFuncionario.GerarCracha()
+        {
+            if (CrachaGerado != null)
+            {
+                CrachaGerado(this, new EventArgs());
+            }
+        }
+
+        void IPlantonista.GerarCracha()
+        {
+            if (CrachaGerado != null)
+            {
+                CrachaGerado(this, new EventArgs());
+            }
+        }
+
+        public decimal Salario { get; }
+
+        int IFuncionario.CargaHorariaMensal { get; set; }
+
+        int IPlantonista.CargaHorariaMensal { get; set; }
+
+        public Funcionario(decimal salario)
+        {
+            Salario = salario;
+        }
+
+        public void EfeturarPagamento()
+        {
+            Console.WriteLine("Pagamento Efetuado");
+        }
+    }
+
+    // Sela o tipo, impedindo que uma classe herde dela
+    sealed class Cliente : Pessoa
+    {
+        public DateTime? DataUltimaCompra { get; set; }
+        public decimal? ValorUltimaCompra { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Data última compra: {DataUltimaCompra}";
+        }
+    }
+
+    abstract class Pessoa
+    {
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
