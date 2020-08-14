3965064a8e75e9df54e92c2c8c9b0ddceb1ddf6e
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2020-08-13 22:26:15 -0300

    Curso 7 C# Programming - Aula 3

--- CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 7 - Eventos e LINQ/01/01.03/Program.cs
+++ CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 7 - Eventos e LINQ/01/01.03/Program.cs
@@ -4,9 +4,35 @@ namespace _01_03
 {
     class Program
     {
+        delegate int Operacao(int a, int b);
+        
         static void Main(string[] args)
         {
+            int a = 3;
+            int b = 2;
+             
+            //var operacao = new Operacao(Somar); // outra forma de criar 
+            Operacao operacao = Somar;
+            
+            Console.WriteLine($"{operacao(a, b)}");
+            
+            operacao = Subtrair; 
+            
+            Console.WriteLine($"{operacao(a, b)}");
+            
             Console.ReadKey();
+        }
+
+        public static int Somar(int a, int b)
+        {
+            Console.WriteLine($"A operação Somar foi chamada com a = {a} e b = {b}");
+            return a + b;
+        }
+
+        public static int Subtrair(int a, int b)
+        {
+            Console.WriteLine($"A operação Subtrair foi chamada com a = {a} e b = {b}");
+            return a - b;
         }
     }
 }
--- CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 7 - Eventos e LINQ/01/01.04/Program.cs
+++ CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 7 - Eventos e LINQ/01/01.04/Program.cs
@@ -4,8 +4,13 @@ namespace _01_04
 {
     class Program
     {
+        delegate int Operacao(int a, int b);
+
         static void Main(string[] args)
         {
+            Operacao operacao = (a, b) => a + b;
+
+            Console.WriteLine(operacao(3, 2));
         }
     }
 }
