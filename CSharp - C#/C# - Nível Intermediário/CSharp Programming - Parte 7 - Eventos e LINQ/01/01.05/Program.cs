3ccd37ec382da15e68e56bf5eb5c9ca7e56f18fa
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2020-08-13 23:02:01 -0300

    Curso 7 C# Programming - Aula 4

--- CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 7 - Eventos e LINQ/01/01.05/Program.cs
+++ CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 7 - Eventos e LINQ/01/01.05/Program.cs
@@ -6,8 +6,45 @@ namespace _01_05
 {
     class Program
     {
+        delegate int Operacao(int a, int b);
+
         static void Main(string[] args)
         {
+            Operacao operacao = (a, b) => a + b;
+
+            
+            // Func é um delegado nativo que retorna valor
+            // o último tipo de um delegado de função é seu retorno
+            // int - parâmetro
+            // int - parâmetro
+            // int - retorno
+            Func<int, int, int> somar = (x, y) => x + y;
+            Console.WriteLine(somar(3, 2));
+
+            // Delegado de ação
+            // não retorna valor
+            Action<string> imprimirMensagem
+                = (mensagem) =>
+                {
+                    Console.WriteLine(mensagem);
+                };
+
+            imprimirMensagem("Olá, Alura!");
+
+
+            var numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Predicate é um delegado especializado em retornar booleanos
+            Predicate<int> divisivelPor3 = (numero) => numero % 3 == 0;
+
+            var divisiveis = Array.FindAll(numeros, divisivelPor3);
+            
+            foreach (var numero in divisiveis)
+            {
+                Console.WriteLine(numero);
+            }
+            
+            Console.WriteLine(operacao(3, 2));
         }
     }
 }
