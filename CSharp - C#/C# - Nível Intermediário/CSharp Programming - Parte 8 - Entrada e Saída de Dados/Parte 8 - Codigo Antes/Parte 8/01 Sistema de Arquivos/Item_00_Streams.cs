b6829de34148964d4728920c99f594d42c3e0170
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2020-08-18 22:44:00 -0300

    Curso 8 C# Programming - Aula 1

--- CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 8 - Entrada e Saída de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_00_Streams.cs
+++ CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 8 - Entrada e Saída de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_00_Streams.cs
@@ -1,16 +1,49 @@
 ﻿using System;
+using System.IO;
 
 namespace Listings
 {
     class Item_00_Streams
     {
-        static void Main(string[] args)
+        static void XMain(string[] args)
         {
             //TAREFAS:
             //1. ABRIR O ARQUIVO Diretores.txt
             //2. LER 10 BYTES DO ARQUIVO
             //3. IMPRIMIR ESSES BYTES NO CONSOLE
 
+            var fluxoDeArquivo = new FileStream("Diretores.txt", FileMode.Open, FileAccess.Read);
+            
+            byte[] array = new byte[10];
+            int posicao = 0;
+            int tamanho = 10;
+            
+            // PRIMEIRA LEITURA
+            fluxoDeArquivo.Read(array, posicao, tamanho);
+            
+            foreach (var caractere in array)
+            {
+                Console.Write((char)caractere);
+            }
+
+            // SEGUNDA LEITURA
+            // Salto relativo = relativo à posição atual
+            // Salto absoluto = relativo à posição inicial do arquivo
+            fluxoDeArquivo.Seek(5, SeekOrigin.Begin);
+
+            // outra forma de fazer é definir a posição do arquivo
+            fluxoDeArquivo.Position = 5;
+                
+            fluxoDeArquivo.Read(array, posicao, tamanho);
+            
+            foreach (var caractere in array)
+            {
+                Console.Write((char)caractere);
+            }
+            
+            fluxoDeArquivo.Dispose();
+            
+            Console.ReadKey();
         }
     }
 }
--- CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 8 - Entrada e Saída de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_01_FileStream.cs
+++ CSharp - C#/C# - Nível Intermediário/CSharp Programming - Parte 8 - Entrada e Saída de Dados/Parte 8 - Codigo Antes/Parte 8/01 Sistema de Arquivos/Item_01_FileStream.cs
@@ -6,8 +6,36 @@ namespace Listings
 {
     class Item_01_FileStream
     {
-        static void XMain(string[] args)
+        static void Main(string[] args)
         {
+
+            FileStream fluxoSaida = new FileStream("ArquivoSaida.txt", FileMode.Create, FileAccess.Write);
+
+            string mensagemSaida = "Olá. Alura!";
+
+            byte[] array = Encoding.UTF8.GetBytes(mensagemSaida);
+            int posicao = 0;
+            int tamanho = mensagemSaida.Length;
+            
+            fluxoSaida.Write(array, posicao, tamanho);
+            fluxoSaida.Close();
+
+            fluxoSaida.Dispose();
+
+            
+            FileStream fluxoEntrada = new FileStream("ArquivoSaida.txt", FileMode.Open, FileAccess.Read);
+
+            byte[] bytesLidos = new byte[fluxoEntrada.Length];
+
+            fluxoEntrada.Read(bytesLidos, posicao, (int)fluxoEntrada.Length);
+
+            var texto = Encoding.UTF8.GetString(bytesLidos);
+            Console.WriteLine("Mensagem Lida: " + texto);
+
+            fluxoEntrada.Dispose();
+            
+            
+            Console.ReadKey();
         }
     }
 }
