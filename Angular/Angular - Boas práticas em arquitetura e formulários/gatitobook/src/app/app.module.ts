9825f8d66380bbc8a8535b00925f7f2e8e08446e
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2021-07-01 19:55:54 -0300

    Curso 2 Angular - Boas práticas em arquiteturas e formulários

---
+++ Angular/Angular - Boas práticas em arquitetura e formulários/gatitobook/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { RodapeModule } from './componentes/rodape/rodape.module';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
