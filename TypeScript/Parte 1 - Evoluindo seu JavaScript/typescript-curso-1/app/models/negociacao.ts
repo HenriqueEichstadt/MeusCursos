0927d78ef735e428147ed91b990691cd178f3853
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2021-08-20 16:29:21 -0300

    Finalizando curso 1 de TypeScript

---
+++ TypeScript/Parte 1 - Evoluindo seu JavaScript/typescript-curso-1/app/models/negociacao.ts
@@ -0,0 +1,18 @@
+export class Negociacao {
+
+    constructor(
+        public _data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number
+        ) { }
+
+    get volume(): number {
+        return this.quantidade * this.valor;
+    }
+
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return data;
+    }
+
+}
