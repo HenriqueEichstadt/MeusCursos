18db4ae7dab7ac1eedb95f32f352e1f49da2ed73
Henrique Alexsander Eichstadt <henrique98ti@gmail.com>
2025-09-20 23:10:41 -0300

    Avaliando pacote nuget Microsoft.SemanticKernel

---
+++ Inteligência Artificial/MicrosoftSemanticKernelTests/MicrosoftSemanticKernelTests/Plugins/ProductPlugin.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftSemanticKernelTests.Plugins
+{
+    public class ProductPlugin
+    {
+        private readonly List<Product> _products =
+        [
+            new(1, "Mousepad", true, 10),
+            new(2, "Mouse Gamer", true, 8),
+            new(3, "Teclado Gamer", true, 1),
+            new(4, "Capa Monitor", false, 1),
+            new(5, "Monitor Gamer", true, 5),
+        ];
+
+        [KernelFunction("get_products")]
+        [Description("Returns list of products")]
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            await Task.Delay(1);
+            return _products;
+        }
+
+        [KernelFunction("get_state")]
+        [Description("Get the state of a particular product")]
+        public async Task<Product?> GetStateAsync([Description("The ID of the product")] int id)
+        {
+            await Task.Delay(1);
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+
+
+        [KernelFunction("change_state")]
+        [Description("Change the state of the product")]
+        public async Task<Product?> ChangeStateAsync(
+            [Description("The ID of the product")] int id,
+            [Description("The content of the product to be modified")] Product model)
+        {
+            await Task.Delay(1);
+            var product = _products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return null;
+
+            product = new Product(model.Id, model.Title, model.IsActive, model.QuantityOnHand);
+
+            return product;
+        }
+    }
+}
+
+public record Product(int Id, string Title, bool IsActive, int QuantityOnHand);
---
+++ Inteligência Artificial/MicrosoftSemanticKernelTests/MicrosoftSemanticKernelTests/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using MicrosoftSemanticKernelTests.Plugins;
+
+namespace MicrosoftSemanticKernelTests;
+
+public class Program
+{
+    public async static Task Main(string[] args)
+    {
+        var builder = Kernel.CreateBuilder()
+            /*.AddOllamaChatCompletion(
+                modelId: "llama3.1:latest",
+                endpoint: new Uri("http://localhost:11434")
+            )*/
+            .AddOpenAIChatCompletion(
+                modelId: "gpt-4o-mini",
+                apiKey: "OPEN_AI_TOKEN"
+            )
+            ;
+
+        builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+        var app = builder.Build();
+
+        var chatCompletionService = app.GetRequiredService<IChatCompletionService>();
+
+        app.Plugins.AddFromType<ProductPlugin>("Plugins");
+
+        OpenAIPromptExecutionSettings settings = new()
+        {
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+        };
+
+        var history = new ChatHistory();
+
+        string? input;
+        do
+        {
+          
+            Console.Write("User > ");
+            input = Console.ReadLine();
+
+            history.AddUserMessage(input);
+
+            ChatMessageContent? result = await chatCompletionService.GetChatMessageContentAsync(
+                history, 
+                settings, 
+                kernel: app
+                );
+            Console.WriteLine("Assistant > " + result);
+
+            history.AddMessage(result.Role, result.Content ?? string.Empty);
+        }
+        while (input is not null);
+
+    }
+}
